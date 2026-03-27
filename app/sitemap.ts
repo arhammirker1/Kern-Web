bf42434b9bf4a7bce89b3b13212b7f2ef10a9165
Arham Mirkar <arhammirker1@gmail.com>
2026-03-28 00:00:38 +0500

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://kobin.team',
+      url: 'https://www.kobin.team/',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
