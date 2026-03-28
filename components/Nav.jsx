21c81618841d72891d95d6609fb4250ca0b8b1f4
Arham Mirkar <arhammirker1@gmail.com>
2026-03-28 09:05:33 +0500

    logo test

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav({ activePage }) {
   return (
     <nav>
       <Link href="/" className="nav-logo">
-        <img src="/dark_cream.svg" width="28" height="28" alt="Kobin" />
+        <img src="/logo_1.png" width="28" height="28" alt="Kobin" />
         <span className="nav-wordmark">Kobin</span>
       </Link>
       <div className="nav-links">
