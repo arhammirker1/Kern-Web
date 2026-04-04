fa362a195e260cdcd120ab2293945263830c9188
Arham Mirkar <arhammirker1@gmail.com>
2026-04-04 14:59:31 +0500

    Update page.tsx

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function ThankYouPage() {
   return (
     <>
-      <Nav />
+      <Nav activePage="" />
       <div style={{
         minHeight: '100vh',
         display: 'flex',
